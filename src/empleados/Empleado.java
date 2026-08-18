7c56d7946b40785152a53f4a06454de9202ac49c
EMMAXZZ <emmnuelpinda@gmail.com>
2026-08-17 21:04:01 -0600

    modificacion de la clase Empleado
    se añadio sus atributos y sus metodos set y get

--- src/empleados/Empleado.java
+++ src/empleados/Empleado.java
@@ -9,5 +9,41 @@ package empleados;
  * @author EMMAXZZ
  */
 public class Empleado {
+    private String ID;
+    private String Nombre;
+    private String Puesto;
+    private double Salario;
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getPuesto() {
+        return Puesto;
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public void setPuesto(String Puesto) {
+        this.Puesto = Puesto;
+    }
+
+    public void setSalario(double Salario) {
+        this.Salario = Salario;
+    }
+
+    public Empleado(String ID, String Nombre, String Puesto, double Salario) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+        this.Puesto = Puesto;
+        this.Salario = Salario;
+    }
     
+   
 }
