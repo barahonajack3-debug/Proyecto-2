b37be229fb38e237524d5b617f343160dd647f69
EMMAXZZ <emmnuelpinda@gmail.com>
2026-08-19 23:02:39 -0600

    se creo algunos metodos y excepciones
     se creo metodos agregar,buscar y eliminar empleados y con algunas excepciones como, id duplicada al momento de agregar y empleado no encontrado al momento de eliminar

--- src/empleados/gestorEmpleado.java
+++ src/empleados/gestorEmpleado.java
@@ -3,11 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package empleados;
-
+import excepciones.EmpleadoNoencontradoException;
+import excepciones.IDduplicadaException;
+import java.util.ArrayList;
 /**
  *
  * @author EMMAXZZ
  */
 public class gestorEmpleado {
+    
+    private ArrayList<Empleado> empleados;
+    
+    public gestorEmpleado(){
+    empleados = new ArrayList<>();
+}
+    public void agregarEmpleado(Empleado empleado) 
+      throws IDduplicadaException {
+       for (Empleado e : empleados) {
+          if (e.getID().equals(empleado.getID())) {
+            throw new IDduplicadaException(
+             "Ya existe un empleado con esa identificación"
+                );
+            }
+        }
+        empleados.add(empleado);
+    } 
+    public Empleado buscarEmpleado(String ID){
+        
+       for(Empleado empleado : empleados){
+           if(empleado.getID().equals(ID)){
+               return empleado;
+           }
+       }
+      return null;
+    }
+    
+    public void eliminarEmpleado(String ID)
+        throws EmpleadoNoencontradoException{
+        Empleado empleado = buscarEmpleado(ID);
+       if (empleado == null){
+          throw new EmpleadoNoencontradoException(
+                  "No existe empleado con ese ID"
+          );
+       }
+       empleados.remove(empleado);
+    }
     
 }
---
+++ src/excepciones/IDduplicadaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author EMMAXZZ
+ */
+public class IDduplicadaException extends Exception {
+    
+    public IDduplicadaException(String mensaje){
+        super(mensaje);
+    }
+}
