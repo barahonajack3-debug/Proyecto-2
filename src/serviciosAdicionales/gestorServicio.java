7633f652987b7cdd28680f7408928521db7e0960
EMMAXZZ <emmnuelpinda@gmail.com>
2026-08-23 21:26:48 -0600

    clase gestorServicio
    se agrego metodos de agregar,buscar,actulizar y eliminar

--- src/serviciosAdicionales/gestorServicio.java
+++ src/serviciosAdicionales/gestorServicio.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package serviciosAdicionales;
-
+import java.util.ArrayList;
 /**
  *
  * @author EMMAXZZ
  */
 public class gestorServicio {
-    
+     private ArrayList<ServicioAdicional> servicios;
+       
+     public gestorServicio() {
+        servicios = new ArrayList<>();
+    }
+     
+     public void agregarServicio(ServicioAdicional servicio){
+         servicios.add(servicio);
+     }
+     
+     public ServicioAdicional buscarServicio(int codigo){
+         for (ServicioAdicional servicio : servicios) {
+             if (servicio.getCodigo() == codigo) {
+                return servicio;
+        
+         }
+     }
+         return null;
+  }
+     
+     public void actualizarServicio(int codigo, String descripcion, double precio) {
+        ServicioAdicional servicio = buscarServicio(codigo);
+        if (servicio != null) {
+            servicio.setDescripcion(descripcion);
+            servicio.setPrecio(precio);
+        }
+    }
+      public void eliminarServicio(int codigo) {
+        ServicioAdicional servicio = buscarServicio(codigo);
+        if (servicio != null) {
+            servicios.remove(servicio);
+        }
+    }
+
+    public ArrayList<ServicioAdicional> getServicios() {
+        return servicios;
+    }
 }
