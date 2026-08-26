fbdf368f52bbc0f6a01a0a215c92f59b3c76fc0b
DarioRodriguez09 <darioroel22@gmail.com>
2026-08-25 21:24:26 -0600

    Se creo una clase en excepciones
    Se integro la excpecion de ClienteConContratosVinculadosException

---
+++ src/excepciones/ClienteConContratosVinculadosException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author Dario R
+ */
+public class ClienteConContratosVinculadosException extends CajaAlmecenamientoException{
+    public ClienteConContratosVinculadosException(String mensaje){
+        super(mensaje);
+    }
+    
+}
