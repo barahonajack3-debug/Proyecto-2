c742aa8260a944237c01b4fdd6a2519c44d9f27f
DarioRodriguez09 <darioroel22@gmail.com>
2026-08-25 20:12:28 -0600

    Cambios en la Cliente
    Se agregaron elementos en la clase

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -4,10 +4,55 @@
  */
 package clientes;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  *
  * @author Dario R
  */
-public class Cliente {
+public class Cliente extends Persona {
+    private final LocalDate fechaNacimiento;
+    private String correoElectronico;
+
+    public Cliente(LocalDate fechaNacimiento, String correoElectronico, String identificacion, String nombreCompleto, String telefono) {
+        super(identificacion, nombreCompleto, telefono);
+        this.fechaNacimiento = fechaNacimiento;
+        this.correoElectronico = correoElectronico;
+        validarFechaNacimiento(fechaNacimiento);
+        validarMayorEdad(fechaNacimiento);
+        validarCorreo(correoElectronico);
+        
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    public int calcularEdad(){
+       return Period.between(fechaNacimiento, LocalDate.now()).getYears();    
+   }
+    private static void validarFechaNacimiento (LocalDate fechaNacimiento){
+        if (fechaNacimiento == null)
+            throw new IllegalArgumentException("La fecha de nacimiento es obligatoria ");
+    }
     
+    private static void validarMayorEdad(LocalDate fechaNacimiento) {
+    int edad = Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    if (edad < 18) {
+        throw new IllegalArgumentException("El cliente debe ser mayor de edad (18 años o más).");
+    }
+  }
+    private void validarCorreo(String correo){
+        if (correo == null){
+            throw new IllegalArgumentException ("El correo es obligatorio");
+        }
+    }
 }
+    
